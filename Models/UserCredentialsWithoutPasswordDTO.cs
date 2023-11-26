bb6509285a313de33a71e8c9e30201c03ed61a2a
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-26 10:46:24 +0200

    Created user credentials without including the password

---
+++ Models/UserCredentialsWithoutPasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApiWithJwt.Models
+{
+    public class UserCredentialsWithoutPasswordDTO
+    {
+        public Guid Id = Guid.NewGuid();
+        public string Username { get; set; }
+        public string FavoriteDrink { get; set; }
+    }
+}
