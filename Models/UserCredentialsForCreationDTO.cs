8a048931546fa042cf49fecaaac06b84f08c4200
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-26 10:45:17 +0200

    Created DTO for creating new user

---
+++ Models/UserCredentialsForCreationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApiWithJwt.Models
+{
+    public class UserCredentialsForCreationDTO
+    {
+        public string Username { get; set; }
+        public string FavoriteDrink { get; set; }
+        public string Password { get; set; }
+    }
+}
