c05435aad48962ec0e6a6d061d09077da1906c21
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-28 12:37:13 +0200

    Added [Required] when needed

--- Models/UserLoginDTO.cs
+++ Models/UserLoginDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace MinimalApiWithJwt.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalApiWithJwt.Models
 {
     public class UserLoginDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- Models/UserRegisterDTO.cs
+++ Models/UserRegisterDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace MinimalApiWithJwt.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalApiWithJwt.Models
 {
     public class UserRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
         public string FavoriteDrink { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
