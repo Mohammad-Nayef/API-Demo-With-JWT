153c05bfe9ee0a5d011ff26b40291e7666d53483
mohammad-nayef <awladmohammad20@gmail.com>
2023-11-28 11:49:06 +0200

    Created a user for registeration

---
+++ Models/UserRegisterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalApiWithJwt.Models
+{
+    public class UserRegisterDTO
+    {
+        public string Username { get; set; }
+        public string FavoriteDrink { get; set; }
+        public string Password { get; set; }
+    }
+}
